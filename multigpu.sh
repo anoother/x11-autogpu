b7159ded0861a057836a187b84ae7327c11c88bb
Ahmad Jagot <ahmadjagot@gmail.com>
2017-04-02 03:01:10 +0100

    Script works. Yet to test the unit file.

--- multigpu.sh
+++ multigpu.sh
@@ -1,14 +1,63 @@
 #!/bin/bash
 
-cards="$(lspci | grep -E '^[0-9:.]+ VGA compatible controller')"
+set -u
 
-function generate_config() {
-    id=$1
-    echo $id
-    shift
-    echo $@
+OUTPUT_FILE="/etc/X11/xorg.conf.d/09-multigpu.conf"
+SNIPPETS_FILE="/etc/multigpu.snippets"
+ENUMERATE_UNIQUE_CARDS=0 # Enumeration not yet implemented
+ACTION=$1
+
+function get_config_snippet() {
+    to_match="$@"
+    while IFS= read line; do
+        if [[ "$line" =~ ^[[:space:]]+ ]]; then
+            # This is a config option
+            if [ ! -z "$print_config" ]; then
+                echo "$line"
+            fi
+        else
+            # This is a new device section
+            if [[ "$to_match" =~ $line ]]; then
+                print_config=something
+            else
+                print_config=""
+            fi
+        fi
+    done < "$SNIPPETS_FILE"
 }
 
-echo "$cards" | while read card; do
-    generate_config $card
-done
+function generate_config() {
+    pci_id=$1
+    shift
+    device_string="$@"
+    echo "Section \"Device\""
+    echo "    BusID       PCI:${pci_id}\""
+    get_config_snippet $device_string
+    echo 'EndSection'
+    echo # newline
+}
+
+function main() {
+    case $ACTION in
+        noop)
+            OUTPUT_FILE=/dev/stdout
+            ;;
+        uninstall)
+            rm -v "$OUTPUT_FILE"
+            ;;
+        install|reinstall)
+            :
+            ;;
+        *)
+            echo "Please provide an option."
+            ;;
+    esac
+    : > "$OUTPUT_FILE"
+    cards="$(lspci | grep -E '^[0-9:.]+ VGA compatible controller')"
+    echo "$cards" | while read card; do
+        generate_config $card >> "$OUTPUT_FILE"
+    done
+}
+
+main
+
